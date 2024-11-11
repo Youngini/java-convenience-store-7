5c5f7105ff90bfd6135470ae5b1d5fa1f8a61532
young-in <hardworkingini@gmail.com>
2024-11-11 18:31:06 +0900

    feat(Promotion.java) : 프로모션을 저장하기 위한 class 생성

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,19 @@
+package store.domain;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int buy;
+    private int get;
+
+    public Promotion(String name, LocalDate startDate, LocalDate endDate, int buy, int get) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.buy = buy;
+        this.get = get;
+    }
+}
