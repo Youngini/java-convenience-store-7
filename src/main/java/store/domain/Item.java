3f8c79f8cac943d76c93a4b6a008babacf800f92
young-in <hardworkingini@gmail.com>
2024-11-11 19:21:20 +0900

    feat(Items.java) : 사용자가 구매하고 싶은 물품들을 저장하기 위한 class 생성

---
+++ src/main/java/store/domain/Item.java
@@ -0,0 +1,11 @@
+package store.domain;
+
+public class Item {
+    private String name;
+    private String quantity;
+
+    public Item(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/store/domain/Items.java
@@ -0,0 +1,11 @@
+package store.domain;
+
+import java.util.List;
+
+public class Items {
+    private List<Item> items;
+
+    public Items(List<Item> items) {
+        this.items = items;
+    }
+}
